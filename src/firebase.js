f7a28eac28c55488b28d3c026073ea7fde2145b9
emmattfu <makcimdjan@gmail.com>
2020-09-08 23:33:55 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVaFYZcpCBAODgSPU_OkjGb3I7LoheHOY",
+    authDomain: "clone-996ac.firebaseapp.com",
+    databaseURL: "https://clone-996ac.firebaseio.com",
+    projectId: "clone-996ac",
+    storageBucket: "clone-996ac.appspot.com",
+    messagingSenderId: "712569644093",
+    appId: "1:712569644093:web:dcf5788f057eba14e23654",
+    measurementId: "G-JNW114G3DK"
+  };
