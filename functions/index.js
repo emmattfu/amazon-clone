527f593a6900de2d60d49fb93fcf4c874b5a54f7
emmattfu <makcimdjan@gmail.com>
2020-09-21 15:23:59 +0300

    express

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,20 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const { request } = require('express');
+const stripe = require('stripe')('sk_test_51HT0EgLDM9Q25ThyZPYUis2aU6kB05xjIzTjgTSIgAgj3247WrLIY0CKMvPmBXKygSIvUipDgeUR6maVIsN6YMYl00MVdKLwSk');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// API
+
+// App config
+const app = express();
+
+// Middlewares
+app.use(cors({origin: true}));
+app.use(express.json());
+
+// API routes
+app.get('/', (request, response) => response.status(200).send('hello world'))
+
+//Listen command
+exports.api = functions.https.onRequest(app)
