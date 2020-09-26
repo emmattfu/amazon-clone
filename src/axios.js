ce2f4fe9e918999dfdf8a0b685bb762f98eaf6f2
emmattfu <makcimdjan@gmail.com>
2020-09-26 14:04:49 +0300

    build

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-996ac/us-central1/api' // THE API URL
+    baseURL: 'https://us-central1-clone-996ac.cloudfunctions.net/api' // THE API URL
 })
 
 export default instance
